ab9853eb8e4c3bb9d255732214154cfa42e715b1
Mark Abramov <markizko@gmail.com>
2011-04-03 12:56:35 -0500

    Jsus inflection utility module containing various string manipulation techniques

--- lib/jsus/util.rb
+++ lib/jsus/util.rb
@@ -3,5 +3,6 @@ module Jsus
     autoload :Tree,       'jsus/util/tree'
     autoload :Documenter, 'jsus/util/documenter'
     autoload :Validator,  'jsus/util/validator'
+    autoload :Inflection, 'jsus/util/inflection'
   end # Util
 end # Jsus
