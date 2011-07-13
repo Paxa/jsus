780824c6f64aff3b71382dc14b74e9bff98bee93
Pavel <pavel.evst@gmail.com>
2011-07-13 18:47:34 +0800

    Revert "Rails init.rb file"
    This reverts commit f87a522e6f611dc262185395e48463846d8c6623.
    
    Conflicts:
    
    	README

--- rails/init.rb
+++
@@ -1,9 +0,0 @@
-Jsus::Middleware.settings = {
-  :cache         => true,
-  :cache_path    => "#{Rails.root}/public/javascripts/jsus/require",
-  :packages_dir  => "#{Rails.root}/public/javascripts/Source",
-  :cache_pool    => false,
-  :includes_root => "#{Rails.root}/public/javascripts/Source"
-}
-
-Rails.configuration.middleware.use Jsus::Middleware
